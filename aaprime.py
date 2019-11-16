a63491937302a9609bef014234529341729b0cfa
Emir <githubemirgul@gmail.com>
2019-11-16 17:32:41 +0300

    first commit

---
+++ aaprime.py
@@ -0,0 +1,18 @@
+def allprimes(x):
+    a = [2]
+    b = 0
+    if x > 1:
+        for l in range(2, x):
+             for i in range(2, l):
+                if l % i == 0:
+                    pass
+                else:
+                    b = +1
+                if b == len(range(2, l)):
+                    a.append(l)
+    else:
+        print("1den büyük bir sayı giriniz.")
+    return a
+if __name__ == "__main__":
+    x  = int(input("Bir sayı giriniz : "))
+    print(allprimes(x))
